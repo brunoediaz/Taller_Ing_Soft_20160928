2802d4a4bb562a706ae9cc7e38d7cb5ca8538475
brunoediaz <brunoediaz18@gmail.com>
2016-09-28 10:50:34 -0300

    Multiplicación y División

--- Calculadora.java
+++ Calculadora.java
@@ -24,7 +24,7 @@ public class Calculadora{
 			
 		}
 		else{
-			System.out.println("Uso: Calculadora operador operando1 operando2");
+			System.out.println("Uso: Calculadora operador (+ - * /) operando1 operando2");
 		}
 	}
 }
